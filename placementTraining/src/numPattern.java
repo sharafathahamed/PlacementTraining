b91a59c1260f8fe44493db52f89290e68c81885b
Sharafath Ahamed S <115935071+sharafathahamed@users.noreply.github.com>
2025-02-18 17:03:07 +0530

    Add files via upload

---
+++ placementTraining/src/numPattern.java
@@ -0,0 +1,12 @@
+public class numPattern {
+    public static void main(String[] args) {
+        int row = 5;
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+    }
+
+    }
+}
---
+++ placementTraining/src/pattern2.java
@@ -0,0 +1,13 @@
+public class pattern2 {
+    public static void main(String[] args) {
+        int rows=5;
+        for (int i=1;i<=rows;i++){
+            for(int j=1;j<=3;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
